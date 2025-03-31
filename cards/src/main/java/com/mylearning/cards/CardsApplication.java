fa8da73f396e9b86c8f101003d37ebebfcfc8b0e
saichandkyama <saichandkyama1993@gmail.com>
2025-03-31 14:39:40 -0500

    unnecessary changes

--- cards/src/main/java/com/mylearning/cards/CardsApplication.java
+++ cards/src/main/java/com/mylearning/cards/CardsApplication.java
@@ -17,21 +17,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Cards microservice REST API Documentation",
-				description = "EazyBank Cards microservice REST API Documentation",
+				description = "SSP Bank Cards microservice REST API Documentation",
 				version = "v1",
 				contact = @Contact(
-						name = "Madan Reddy",
-						email = "tutor@eazybytes.com",
-						url = "https://www.eazybytes.com"
+						name = "Saichand K",
+						email = "saichand@gmail.com",
+						url = "https://www.saichandportfolio.com"
 				),
 				license = @License(
 						name = "Apache 2.0",
-						url = "https://www.eazybytes.com"
+						url = "https://www.sspbank.com"
 				)
 		),
 		externalDocs = @ExternalDocumentation(
-				description = "EazyBank Cards microservice REST API Documentation",
-				url = "https://www.eazybytes.com/swagger-ui.html"
+				description = "SSP Bank Cards microservice REST API Documentation",
+				url = "https://www.sspbank.com/swagger-ui.html"
 		)
 )
 public class CardsApplication {
